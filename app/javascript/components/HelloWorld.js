543d8e6214db1910fad8b12f5a04d92fd3515641
bps_saito <bps_saito@bps-saitonoMacBook-puro.local>
2019-10-23 18:25:45 +0900

    material-uiの導入

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,10 +1,13 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Button from '@material-ui/core/Button'
+
 class HelloWorld extends React.Component {
   render () {
     return (
       <React.Fragment>
         Reactコンポーネントの出力　Greeting: {this.props.greeting}
+        <Button>Material-ui_Core_Button</Button>
       </React.Fragment>
     );
   }
