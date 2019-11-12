1de76b6222906de5c7ec515cc09b11b1cab88026
bps_saito <bps_saito@bps-saitonoMacBook-puro.local>
2019-11-12 11:47:31 +0900

    rails を 6.0 -> 5.2.3に変更

--- vendor/bundle/ruby/2.6.0/gems/ffi-1.11.1/ext/ffi_c/libffi-x86_64-darwin17/include/ffitarget.h
+++
@@ -1,1 +0,0 @@
-/Users/bps_saito/myLabo/ProtectMoney/vendor/bundle/ruby/2.6.0/gems/ffi-1.11.1/ext/ffi_c/libffi/src/x86/ffitarget.h
---
+++ vendor/bundle/ruby/2.6.0/gems/ffi-1.11.2/ext/ffi_c/libffi-x86_64-darwin17/include/ffitarget.h
@@ -0,0 +1,1 @@
+/Users/bps_saito/myLabo/ProtectMoney/vendor/bundle/ruby/2.6.0/gems/ffi-1.11.2/ext/ffi_c/libffi/src/x86/ffitarget.h
